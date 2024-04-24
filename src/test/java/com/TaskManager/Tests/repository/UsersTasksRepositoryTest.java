f027cb91e88ac327cc1878df09a2ea1ee74423db
bnino <brayannir1993@gmail.com>
2024-04-24 10:10:53 -0500

    Search by user assigned id in tasks_users

---
+++ src/test/java/com/TaskManager/Tests/repository/UsersTasksRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.TaskManager.Tests.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.TaskManager.repository.UsersTasksRepository;
+
+public class UsersTasksRepositoryTest {
+
+    @Autowired
+    UsersTasksRepository usersTasksRepository;
+
+    @Test
+    void testFindUsersTasksByIdUserAssigned() {
+
+    }
+}
